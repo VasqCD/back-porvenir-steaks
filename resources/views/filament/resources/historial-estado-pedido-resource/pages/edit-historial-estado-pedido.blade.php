5f827af9c7ffb4ebe86f3096c8be7df0dd0dfb68
Christian Vasquez <VasqCD@users.noreply.github.com>
2025-04-08 15:37:36 -0600

    refactor: Simplifica la estructura de las páginas de creación y edición de historial de estado de pedido

--- resources/views/filament/resources/historial-estado-pedido-resource/pages/edit-historial-estado-pedido.blade.php
+++ resources/views/filament/resources/historial-estado-pedido-resource/pages/edit-historial-estado-pedido.blade.php
@@ -1,20 +1,18 @@
-{{-- resources/views/filament/resources/historial-estado-pedido-resource/pages/edit-historial-estado-pedido.blade.php --}}
-<x-filament::page
-    :class="static::getPageClass()">
-    <x-filament::form wire:submit.prevent="save">
-        {{ $this->form }}
+<x-filament::page>
+    {{ $this->form }}
 
-        <x-filament::page.actions
-            :actions="$this->getCachedFormActions()"
-            :full-width="$this->hasFullWidthFormActions()" />
+    <x-filament::actions
+        :actions="$this->getActions()"
+        :full-width="$this->hasFullWidthActions()"
+    />
 
-        @if($this->record->pedido)
+    @if($this->record->pedido)
         <x-filament::section class="mt-6">
             <x-slot name="heading">
                 Historial completo del pedido #{{ $this->record->pedido_id }}
             </x-slot>
-
+            
             @include('filament.components.historial-estados-avanzado', ['historial' => $this->record->pedido->historialEstados()->with('usuario')->orderBy('fecha_cambio', 'desc')->get()])
         </x-filament::section>
-        @endif
+    @endif
 </x-filament::page>
